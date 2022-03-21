23323d7883e1ee80d3da292ca76952451fc21d79
gmofishsauce <pdxjjb@gmail.com>
2022-03-20 17:55:35 -0700

    Add engine file and unit test

--- lib/gtw.go
+++
@@ -1,9 +0,0 @@
-/*
-Package gtw implements a word game.
-
-Artifacts in this package are suitable for use when
-implementing a user interface for the word game or
-when creating bots to play the word game.
-
-*/
-package gtw
